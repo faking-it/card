2e9f0dd5d88663a9359d19b19974851e12364bfb
Bruno <michelbruno453@hotmail.com>
2020-05-07 15:09:56 +0200

    version 1.0.1

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 
 const boxen = require("boxen");
+const chalk = require("chalk");
 
 //Text data
 const data = {
-  name: "                                   Bruno Michel",
-  handle: "faking-it       ",
-  work: " Junior developer at BeCode.org",
-  facebook: " https://www.facebook.com/bruno.michel.10/",
-  instagram: " https://www.instagram.com/brugiusan/",
-  npm: " https://www.npmjs.com/~faking-it",
-  github: " https://github.com/faking-it",
-  linkedin: " https://www.linkedin.com/in/bruno-michel/",
+  name: "                              Bruno Michel",
+  handle: "faking-it     ",
+  work: " Junior developer at " + chalk.bold("BeCode.org"),
+  facebook: " https://www.facebook.com/" + chalk.red("bruno.michel.10"),
+  instagram: " https://www.instagram.com/" + chalk.red("brugiusan"),
+  npm: " https://www.npmjs.com/" + chalk.red("~faking-it"),
+  github: " https://github.com/" + chalk.red("faking-it"),
+  linkedin: " https://www.linkedin.com/in/" + chalk.red("bruno-michel"),
 
-  labelWork: "            Work:",
-  labelFacebook: "        Facebook:",
-  labelInstagram: "       Instagram:",
-  labelNpm: "             npm:",
-  labelGitHub: "          GitHub:",
-  labelLinkedIn: "        LinkedIn:"
+  labelWork: "      Work:",
+  labelFacebook: "  Facebook:",
+  labelInstagram: " Instagram:",
+  labelNpm: "       npm:",
+  labelGitHub: "    GitHub:",
+  labelLinkedIn: "  LinkedIn:"
 };
 
 const newline = "\n";
@@ -28,19 +29,20 @@ const emptyline = "";
 console.log(
   boxen(
     [
-      data.name + " / " + data.handle,
+      data.name + " / " + chalk.red(data.handle),
       emptyline,
-      data.labelWork + data.work,
+      chalk.bold(data.labelWork) + data.work,
       emptyline,
-      data.labelFacebook + data.facebook,
-      data.labelInstagram + data.instagram,
-      data.labelNpm + data.npm,
-      data.labelGitHub + data.github,
-      data.labelLinkedIn + data.linkedin
+      chalk.bold(data.labelFacebook) + data.facebook,
+      chalk.bold(data.labelInstagram) + data.instagram,
+      chalk.bold(data.labelNpm) + data.npm,
+      chalk.bold(data.labelGitHub) + data.github,
+      chalk.bold(data.labelLinkedIn) + data.linkedin
     ].join(newline),
 
     {
-      borderStyle: "round"
+      borderStyle: "round",
+      padding: 1
     }
   )
 );
