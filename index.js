3e26de4b75d9b518f53f7f739d6f59842d315321
Bruno <michelbruno453@hotmail.com>
2020-05-07 14:37:12 +0200

    ajout chalk

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 #!/usr/bin/env node
 
-//Texte
-const data = {
-    name: "                                  Bruno Michel",
-    handle: "faking-it",
-    work: "Junior developer at BeCode.org",
-    facebook: "https://www.facebook.com/bruno.michel.10/",
-    instagram: "https://www.instagram.com/brugiusan/",
-    npm: "https://www.npmjs.com/~faking-it",
-    github: "https://github.com/faking-it",
-    linkedin: "https://www.linkedin.com/in/bruno-michel/",
+const boxen = require("boxen");
 
-    labelWork: "            Work:",
-    labelFacebook: "        Facebook:",
-    labelInstagram: "       Instagram:",
-    labelNpm: "             npm:",
-    labelGitHub: "          GitHub:",
-    labelLinkedIn: "        LinkedIn:",
+//Text data
+const data = {
+  name: "                                   Bruno Michel",
+  handle: "faking-it       ",
+  work: " Junior developer at BeCode.org",
+  facebook: " https://www.facebook.com/bruno.michel.10/",
+  instagram: " https://www.instagram.com/brugiusan/",
+  npm: " https://www.npmjs.com/~faking-it",
+  github: " https://github.com/faking-it",
+  linkedin: " https://www.linkedin.com/in/bruno-michel/",
+
+  labelWork: "            Work:",
+  labelFacebook: "        Facebook:",
+  labelInstagram: "       Instagram:",
+  labelNpm: "             npm:",
+  labelGitHub: "          GitHub:",
+  labelLinkedIn: "        LinkedIn:"
 };
 
 const newline = "\n";
 const emptyline = "";
 
+//Body
 console.log(
-
+  boxen(
     [
-        data.name + " / " + data.handle,
-        emptyline,
-        data.labelWork + data.work,
-        emptyline,
-        data.labelFacebook + data.facebook,
-        data.labelInstagram + data.instagram,
-        data.labelNpm + data.npm,
-        data.labelGitHub + data.github,
-        data.labelLinkedIn + data.linkedin,
+      data.name + " / " + data.handle,
+      emptyline,
+      data.labelWork + data.work,
+      emptyline,
+      data.labelFacebook + data.facebook,
+      data.labelInstagram + data.instagram,
+      data.labelNpm + data.npm,
+      data.labelGitHub + data.github,
+      data.labelLinkedIn + data.linkedin
     ].join(newline),
 
+    {
+      borderStyle: "round"
+    }
+  )
 );
